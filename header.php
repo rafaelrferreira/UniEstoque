0a10aefb892019fcd5019e41e820bcd96dbe23b8
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2016-06-17 20:19:24 -0300

    Adicionado o Zendesk widget

--- header.php
+++ header.php
@@ -45,6 +45,12 @@
         mixpanel.init("15be8803faf10246ab8c948f0ab6e5cb");</script>
 <!-- end Mixpanel -->
 
+<!--Widget Zendesk-->
+    <!-- Start of tsestoque Zendesk Widget script -->
+    <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","tsestoque.zendesk.com");
+    /*]]>*/</script>
+    <!-- End of tsestoque Zendesk Widget script -->
+
 </head>
 
 <body>
